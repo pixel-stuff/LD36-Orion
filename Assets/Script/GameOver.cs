b877ba59d0b3b70a5741cc62c7165f52bec2998d
mathias <mathias@marbotic.fr>
2016-08-27 20:46:06 +0200

    Creation game over

---
+++ Assets/Script/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartGameOverAnimation () {
+		StartCoroutine (FadeOutAnim ());
+	}
+
+	#region Coroutine
+	public IEnumerator FadeOutAnim(){
+		float alpha = 0;
+		Color col = this.GetComponent<Image>().color;
+		do{
+			alpha += 0.015f;
+			col.a = alpha;
+			this.GetComponent<Image>().color = col;
+			yield return new WaitForEndOfFrame();
+		}while(alpha <= 1.0f);
+	}
+	#region Coroutine
+}
