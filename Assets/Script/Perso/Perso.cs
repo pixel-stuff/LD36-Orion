59264a6e1543527bee151c0fa807e5ba10de344f
mathias <mathias@marbotic.fr>
2016-08-27 18:48:11 +0200

    Creation de beaucoup de chose

--- Assets/Script/Perso/Perso.cs
+++ Assets/Script/Perso/Perso.cs
@@ -2,20 +2,27 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class EnnemiManager : MonoBehaviour {
+public class Perso : MonoBehaviour {
 
 	private int m_pvMax = 100;
 	public int m_pv = 0;
 	private float m_currentPercentLife;
 
-	public Sprite[] m_ennemiState;
+	[SerializeField]
+	private Image m_bracelet;
+
+	public Sprite[] m_braceletState;
+
+
 
 	void Awake(){
 		m_pv = m_pvMax;
+		m_currentPercentLife = 100 * m_pv / m_pvMax;
 	}
+
 	// Use this for initialization
 	void Start () {
-	
+		this.GetComponent<CanvasRenderer> (). SetColor (Color.black);
 	}
 	
 	// Update is called once per frame
@@ -35,17 +42,17 @@ public class EnnemiManager : MonoBehaviour {
 
 	#region Coroutine
 	public IEnumerator BeingAttack(){
-
+		
 		yield return new WaitForEndOfFrame ();
 		yield return new WaitForSeconds (this.GetComponent<Animator> ().GetCurrentAnimatorClipInfo (0).Length);
 
 		float newPercent = m_pv * 100 / m_pvMax;
 
-		int lastIndex = m_ennemiState.Length - 1 - (int)m_currentPercentLife/((int)m_pvMax / m_ennemiState.Length);
-		int newIndex = m_ennemiState.Length - 1 - (int)newPercent/((int)m_pvMax / m_ennemiState.Length);
+		int lastIndex = m_braceletState.Length - 1 - (int)m_currentPercentLife/((int)m_pvMax / m_braceletState.Length);
+		int newIndex = m_braceletState.Length - 1 - (int)newPercent/((int)m_pvMax / m_braceletState.Length);
 
 		if (lastIndex != newIndex) {
-			this.GetComponent<Image>().sprite = m_ennemiState [newIndex];
+			m_bracelet.sprite = m_braceletState [newIndex];
 		}
 		m_currentPercentLife = newPercent;
 	}
