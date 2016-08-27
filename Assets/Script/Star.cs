27ac70e39fbff44ef0a22f652975c4add1ab9129
pplans <pierre.plans@gmail.com>
2016-08-27 14:09:35 +0200

    Pouet

---
+++ Assets/Script/Star.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StarTools.DrawLine(Vector3.zero, new Vector3(10.0f, 10.0f, 10.0f), Color.black, 0.0f);
+    }
+}
---
+++ Assets/Script/StarTools.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarTools : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color colorStart, Color colorEnd, float duration = 0.2f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.SetColors(colorStart, colorEnd);
+        lr.SetWidth(0.1f, 0.1f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        if(duration>0.0f)
+            GameObject.Destroy(myLine, duration);
+    }
+}
