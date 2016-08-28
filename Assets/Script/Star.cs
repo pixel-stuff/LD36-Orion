e171e3ca9aa43bd37084d025be9f2c2ecf88573c
pplans <pierre.plans@gmail.com>
2016-08-28 10:49:06 +0200

    Last set up for lines

--- Assets/Script/Star.cs
+++ Assets/Script/Star.cs
@@ -5,9 +5,13 @@ public class Star : MonoBehaviour {
 
     public ConstelationStar starOne;
     public ConstelationStar starTwo;
+    public ConstelationStar star3;
+    public ConstelationStar star4;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         StarTools.AddLine(starOne, starTwo);
+        StarTools.AddLine(starOne, star3);
+        StarTools.AddLine(starOne, star4);
     }
 }
