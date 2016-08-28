d04b89ba48c3c882ec63c6d1b71aa89955bcad3b
pplans <pierre.plans@gmail.com>
2016-08-28 18:45:42 +0200

    Hero blinking

---
+++ Assets/Prefab/Hero/HeroBlink.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroBlink : MonoBehaviour {
+
+    public Material _mat = null;
+    
+    public bool SetBlinkColorAndOpacity(Color c, float opacity)
+    {
+        if(_mat!= null)
+        {
+            _mat.SetColor("_BlinkColor", c);
+            _mat.SetFloat("_BlinkOpacity", opacity);
+            return true;
+        }
+        return false;
+    }
+}
